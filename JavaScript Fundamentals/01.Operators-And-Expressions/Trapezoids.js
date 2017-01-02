57647e6e25bb86ff5f2111bd25e6e1255edc187c
Yasen Ivanov <yasen.denchev@gmail.com>
2017-01-02 17:20:55 +0200

    deleted some stuff

--- JavaScript Fundamentals/01.Operators-And-Expressions/Trapezoids.js
+++
@@ -1,9 +0,0 @@
-function solve(args) {
-    let a = Number(args[0]);
-    let b = Number(args[1]);
-    let h = Number(args[2]);
-    let area = ((a + b) * h) / 2;
-    
-    console.log(area.toFixed(7));
-}
-solve(['2', '1', '33']);
