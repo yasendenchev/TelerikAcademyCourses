5db1f79e317584913186e7ed76f852da64b17292
Yasen Ivanov <yasen.denchev@gmail.com>
2017-01-02 17:23:50 +0200

    deleted some stuff

--- JavaScript Fundamentals/01.Operators-And-Expressions/DivideBy5And7.js
+++
@@ -1,9 +0,0 @@
-function solve(args) {
-    let input = args[0];
-    if (input % 5 === 0 && input % 7 === 0) {
-        console.log("true " + input);
-    }
-    else {
-        console.log("false " + input);
-    }
-}
